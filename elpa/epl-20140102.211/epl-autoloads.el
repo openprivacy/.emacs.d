88320134408674a0a863556ee33247699411433e
Fen Labalme <fen@openprivacy.org>
2014-04-01 13:54:50 -0400

    adding yaml-mode; updating lots of packages

--- elpa/epl-20140102.211/epl-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; epl-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("epl-pkg.el" "epl.el") (21201 38689 670234
-;;;;;;  443000))
-
-;;;***
-
-(provide 'epl-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; epl-autoloads.el ends here
