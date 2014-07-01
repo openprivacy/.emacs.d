b64f97da75360da8a165d4e81d64ecdba6635f84
Fen Labalme <fen@openprivacy.org>
2014-07-01 13:54:08 -0400

    updated packages

---
+++ elpa/dash-20140627.2341/dash-autoloads.el
@@ -0,0 +1,18 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("dash-pkg.el" "dash.el") (21426 62687
+;;;;;;  392530 181000))
+
+;;;***
+
+(provide 'dash-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; dash-autoloads.el ends here
---
+++ elpa/git-rebase-mode-20140605.520/git-rebase-mode-autoloads.el
@@ -0,0 +1,36 @@
+;;; git-rebase-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (git-rebase-mode) "git-rebase-mode" "git-rebase-mode.el"
+;;;;;;  (21426 62684 629436 101000))
+;;; Generated autoloads from git-rebase-mode.el
+
+(autoload 'git-rebase-mode "git-rebase-mode" "\
+Major mode for editing of a Git rebase file.
+
+Rebase files are generated when you run 'git rebase -i' or run
+`magit-interactive-rebase'.  They describe how Git should perform
+the rebase.  See the documentation for git-rebase (e.g., by
+running 'man git-rebase' at the command line) for details.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("/git-rebase-todo\\'" . git-rebase-mode))
+
+;;;***
+
+;;;### (autoloads nil nil ("git-rebase-mode-pkg.el") (21426 62684
+;;;;;;  645509 93000))
+
+;;;***
+
+(provide 'git-rebase-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; git-rebase-mode-autoloads.el ends here
