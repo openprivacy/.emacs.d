76e334e1f5cfccebfb27454352b19569b2712c43
Fen Labalme <fen@openprivacy.org>
2014-11-04 01:17:05 -0500

    updated files

---
+++ elpa/flymake-phpcs-20140712.2331/flymake-phpcs-pkg.el
@@ -0,0 +1,1 @@
+(define-package "flymake-phpcs" "20140712.2331" "making flymake work with PHP CodeSniffer" '((flymake-easy "0.9")) :url "https://github.com/senda-akiha/flymake-phpcs/" :keywords '("flymake" "phpcs" "php"))
